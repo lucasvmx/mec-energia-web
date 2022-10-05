fcdf455f936a725aeb701fb1ff48d3772b5894f9
Irwin Schmitt <irwinschmitt@gmail.com>
2022-10-05 18:07:07 -0300

    feat: create card types

---
+++ components/ConsumerUnitCard.tsx
@@ -0,0 +1,114 @@
+import {
+  Badge,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { Receipt, Star, StarOutline, TrendingUp } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+
+interface Pendency {
+  month: number;
+  year: number;
+}
+
+interface ConsumerUnitCardProps {
+  title: string;
+  disabled?: boolean;
+  isFavorite?: boolean;
+  pendencies?: Pendency[];
+}
+
+const ConsumerUnitCard = ({
+  title,
+  disabled = false,
+  isFavorite = false,
+  pendencies = [],
+}: ConsumerUnitCardProps) => {
+  const [longMonth, setLongMonth] = useState("");
+  const [badgeCount, setBadgeCount] = useState(0);
+
+  useEffect(() => {
+    if (pendencies.length == 0) {
+      setBadgeCount(0);
+      return;
+    }
+
+    const { year, month } = pendencies[pendencies.length - 1];
+    const date = new Date(year, month);
+    const longMonth = date.toLocaleString("pt-br", { month: "long" });
+
+    setLongMonth(longMonth);
+    setBadgeCount(pendencies.length - 1);
+  }, [pendencies]);
+
+  return (
+    <Card
+      variant={disabled ? "outlined" : "elevation"}
+      sx={{
+        minHeight: 195,
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {!disabled && (
+        <CardActions>
+          <IconButton color="inherit">
+            {isFavorite ? <Star /> : <StarOutline />}
+          </IconButton>
+        </CardActions>
+      )}
+
+      <CardContent
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "end",
+          pt: 0,
+        }}
+      >
+        <Typography variant="h5">{title}</Typography>
+      </CardContent>
+
+      <Divider />
+
+      <CardActions sx={{ justifyContent: "space-between", minHeight: "56px" }}>
+        {disabled ? (
+          <Box ml={1}>
+            <Typography>Desativado</Typography>
+          </Box>
+        ) : (
+          <>
+            {pendencies.length == 0 ? (
+              <Box ml={1}>
+                <Typography>Em dia</Typography>
+              </Box>
+            ) : (
+              <Button variant="outlined">Lançar {longMonth}</Button>
+            )}
+
+            <Box>
+              <IconButton color="inherit">
+                <Badge badgeContent={badgeCount} color="warning">
+                  <Receipt />
+                </Badge>
+              </IconButton>
+
+              <IconButton color="inherit">
+                <TrendingUp />
+              </IconButton>
+            </Box>
+          </>
+        )}
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ConsumerUnitCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Box, Container, Grid } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
 
-import UCCard from "../UCCard";
+import ConsumerUnitCard from "../components/ConsumerUnitCard";
 
 const Home: NextPage = () => {
   return (
@@ -15,23 +15,25 @@ const Home: NextPage = () => {
         <Box py={2}>
           <Grid container spacing={3}>
             <Grid item xs={12} sm={6} md={4} lg={3}>
-              <UCCard />
+              <ConsumerUnitCard
+                title="Campus Darcy Ribeiro Gleba A"
+                pendencies={[
+                  { month: 11, year: 2022 },
+                  { month: 10, year: 2022 },
+                ]}
+              />
             </Grid>
 
             <Grid item xs={12} sm={6} md={4} lg={3}>
-              <UCCard />
+              <ConsumerUnitCard
+                title="Campus Gama"
+                isFavorite
+                pendencies={[]}
+              />
             </Grid>
 
             <Grid item xs={12} sm={6} md={4} lg={3}>
-              <UCCard />
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <UCCard />
-            </Grid>
-
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <UCCard />
+              <ConsumerUnitCard title="Campus Ceilândia" disabled />
             </Grid>
           </Grid>
         </Box>
