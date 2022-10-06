2c7efe31b2e089b3d44bb51c289f80450f8f54c9
estevaoreis25 <estevaoreis1587@gmail.com>
2022-10-06 12:03:50 -0300

    Integração da sidebar com a navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,66 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import LeftDrawer from "./LeftDrawer";
+import { AppBar, Toolbar, Typography, Drawer, ListItemButton, ListItemIcon, ListItemText, List, IconButton, Box, CssBaseline } from "@mui/material";
+import ViewModuleIcon from '@mui/icons-material/ViewModule';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
 
-const Header = ()=>{
+const drawerWidth = 240;
+const Header = () => {
+  const [openDrawer, setopenDrawer] = useState(false);
   return (
     <div>
       <React.Fragment>
-        <AppBar sx={{display: 'flex', flexDirection:'row'}}>
-          <LeftDrawer/>
-          <Toolbar>
-            <Typography>
-              MEC Eneergia Inteligente
-            </Typography> 
-          </Toolbar>
-        </AppBar>
-      </React.Fragment>
+        <Box display={"flex"}>
+          <CssBaseline />
+          <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
 
-    </div>
+            <Toolbar>
+              <IconButton onClick={() => setopenDrawer(!openDrawer)}>
+                <MenuIcon />
+              </IconButton>
+              <Typography>
+                MEC Eneergia Inteligente
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer open={openDrawer}
+            onClose={() => setopenDrawer(false)}
+            variant="persistent"
+            anchor="left"
+            sx={{
+              width: drawerWidth,
+              flexShrink: 0,
+              [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+            }}>
+            <Toolbar />
+            <Box sx={{ overflow: 'auto' }}>
+              <List>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <ViewModuleIcon />
+                    <ListItemText>Painel</ListItemText>
+                  </ListItemIcon>
+                </ListItemButton>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <ViewModuleIcon />
+                    <ListItemText>Unidades Consumidoras</ListItemText>
+                  </ListItemIcon>
+                </ListItemButton>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <ViewModuleIcon />
+                    <ListItemText>Operadoras</ListItemText>
+                  </ListItemIcon>
+                </ListItemButton>
+                <CssBaseline />
+              </List>
+
+            </Box>
+          </Drawer>
+        </Box>
+      </React.Fragment>
+    </div >
   )
 }
 
--- src/components/LeftDrawer.tsx
+++ src/components/LeftDrawer.tsx
@@ -1,29 +1,42 @@
 import React from "react";
 import { useState } from "react";
-import { Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText, List, IconButton} from "@mui/material";
+import { Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText, List, IconButton, Box } from "@mui/material";
 import ViewModuleIcon from '@mui/icons-material/ViewModule';
 import MenuIcon from '@mui/icons-material/Menu';
-import { spacing } from '@material-ui/system';
 
-const LeftDrawer = ()=>{
-  const [openDrawer, setopenDrawer] = useState(false)
-  return(
+const LeftDrawer = () => {
+  const [openDrawer, setopenDrawer] = useState(false);
+  return (
     <React.Fragment>
       <Drawer open={openDrawer}
-      onClose={()=>setopenDrawer(false)}>
+        onClose={() => setopenDrawer(false)}>
         <List>
           <ListItemButton>
             <ListItemIcon>
-              <ViewModuleIcon/>
+              <ViewModuleIcon />
               <ListItemText>Painel</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <ViewModuleIcon />
+              <ListItemText>Unidades Consumidoras</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <ViewModuleIcon />
+              <ListItemText>Operadoras</ListItemText>
             </ListItemIcon>
           </ListItemButton>
         </List>
       </Drawer>
-      <IconButton sx={{ml:1}} onClick={()=>setopenDrawer(!openDrawer)}>
-        <MenuIcon/>
-      </IconButton>
-    </React.Fragment>
+      <Box ml={1}>
+        <IconButton onClick={() => setopenDrawer(!openDrawer)}>
+          <MenuIcon />
+        </IconButton>
+      </Box>
+    </React.Fragment >
   )
 }
 
