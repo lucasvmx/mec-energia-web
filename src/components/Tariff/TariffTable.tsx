c6cbebdb6e2d23ac0dc90eabc7687a723848fc1c
estevaoreis25 <estevaoreis1587@gmail.com>
2022-11-14 09:30:15 -0300

    table structure creation

--- src/components/Tariff/TariffTable.tsx
+++ src/components/Tariff/TariffTable.tsx
@@ -1,5 +1,6 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import InfoIcon from '@mui/icons-material/Info';
+import React, { Fragment } from 'react'
 
 export const TariffTable = () => {
   return (
@@ -23,8 +24,113 @@ export const TariffTable = () => {
         </Box>
       </Box>
 
+      <TableContainer>
+        <Table>
+          <colgroup>
+            <col style={{ width: '20%' }} />
+            <col style={{ width: '30%' }} />
+            <col style={{ width: '30%' }} />
+            <col style={{ width: '20%' }} />
+          </colgroup>
+          <TableHead >
+            <TableRow sx={{ background: '#555555' }}>
+              <TableCell sx={{ borderStyle: 'solid', borderWidth: '0 1px 0 0', borderColor: '#fff', color: '#fff' }} align='center'>Posto</TableCell>
+              <TableCell sx={{ borderStyle: 'solid', borderWidth: '0 1px 0 0', borderColor: '#fff', color: '#fff' }} align='center'>Modalidade</TableCell>
+              <TableCell sx={{ borderStyle: 'solid', borderWidth: '0 1px 0 0', borderColor: '#fff', color: '#fff' }}>
+                <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+                  <Box display='flex' justifyContent='space-between' alignItems='center' width='30%'>
+                    <Typography>
+                      TUSD
+                    </Typography>
+                    <InfoIcon />
+                  </Box>
+                  <Box display='flex' justifyContent='space-between' width='80%'>
+                    <Box>
+                      R$/kW
+                    </Box>
+                    <Box>
+                      R$/MWh
+                    </Box>
+                  </Box>
+                </Box>
+              </TableCell>
+              <TableCell sx={{ color: '#fff' }}>
+                <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+                  <Box display='flex' justifyContent='space-between' alignItems='center' width='30%'>
+                    <Box>TE</Box> <InfoIcon />
+                  </Box>
+                  <Box>R$/MWh</Box>
+                </Box>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <Fragment>
+              <TableRow sx={{ background: '#999999' }}>
+                <TableCell rowSpan={3} align='center' sx={{ color: '#fff' }}> Azul</TableCell>
+              </TableRow>
+              <TableRow sx={{ background: '#EEEEEE' }}>
+                <TableCell align='center'> Ponta</TableCell>
+                <TableCell>
+                  <Box display='flex' justifyContent='space-between' width='80%' m='0 auto'>
+                    <Box>90,43</Box>
+                    <Box>118,90 </Box>
+                  </Box>
+                </TableCell>
+                <TableCell align='center'>418,67</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align='center'> Fora Ponta</TableCell>
+                <TableCell>
+                  <Box display='flex' justifyContent='space-between' width='80%' m='0 auto'>
+                    <Box>90,43</Box>
+                    <Box>118,90 </Box>
+                  </Box>
+                </TableCell>
+                <TableCell align='center'>418,67</TableCell>
+              </TableRow>
+            </Fragment>
+            <Fragment>
+              <TableRow sx={{ background: '#999999' }}>
+                <TableCell rowSpan={4} align='center' sx={{ color: '#fff' }}>Verde</TableCell>
+              </TableRow>
+              <TableRow sx={{ background: '#EEEEEE' }}>
+                <TableCell align='center'> Ponta</TableCell>
+                <TableCell>
+                  <Box display='flex' justifyContent='space-between' width='80%' m='0 auto'>
+                    <Box>90,43</Box>
+                    <Box>118,90 </Box>
+                  </Box>
+                </TableCell>
+                <TableCell align='center'>418,67</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align='center'> Fora Ponta</TableCell>
+                <TableCell>
+                  <Box display='flex' justifyContent='space-between' width='80%' m='0 auto'>
+                    <Box>90,43</Box>
+                    <Box>118,90 </Box>
+                  </Box>
+                </TableCell>
+                <TableCell align='center'>418,67</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell align='center'> NA</TableCell>
+                <TableCell>
+                  <Box display='flex' justifyContent='space-between' width='80%' m='0 auto'>
+                    <Box>90,43</Box>
+                    <Box>118,90 </Box>
+                  </Box>
+                </TableCell>
+                <TableCell align='center'>418,67</TableCell>
+              </TableRow>
+            </Fragment>
+          </TableBody>
+        </Table>
+      </TableContainer>
 
 
-    </Box>
+
+    </Box >
   )
 }
