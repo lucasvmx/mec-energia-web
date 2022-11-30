df986664ce3ad8c252582ab99c70dc55a0b8cde6
Irwin Schmitt <irwinschmitt@gmail.com>
2022-11-29 23:42:20 -0300

    fix: next and mui link integration

--- src/components/Drawer/ListItem.tsx
+++ src/components/Drawer/ListItem.tsx
@@ -1,5 +1,5 @@
-import { ComponentType } from "react";
-import Link from "next/link";
+import { ComponentType, ReactNode } from "react";
+import NextLink, { LinkProps } from "next/link";
 import {
   ListItem,
   ListItemButton,
@@ -13,9 +13,26 @@ interface DrawerListItemProps {
   text: string;
   Icon: ComponentType<SvgIconProps>;
   active?: boolean;
-  href?: string;
+  href?: LinkProps["href"];
   onClick?: () => void;
 }
+
+interface WrapperLinkProps {
+  href?: LinkProps["href"];
+  children: ReactNode;
+}
+
+const WrapperLink = ({ href, children }: WrapperLinkProps) => {
+  if (href) {
+    return (
+      <NextLink href={href} passHref legacyBehavior>
+        {children}
+      </NextLink>
+    );
+  }
+
+  return <>{children}</>;
+};
 
 const DrawerListItem = ({
   open,
@@ -24,40 +41,43 @@ const DrawerListItem = ({
   active,
   href,
   onClick,
-}: DrawerListItemProps) => (
-  <ListItem disablePadding sx={{ display: "block", p: 1 }}>
-    <ListItemButton
-      sx={{
-        minHeight: 48,
-        justifyContent: open ? "initial" : "center",
-        px: 1.5,
-      }}
-      selected={active}
-      onClick={onClick}
-      {...(href && { href, LinkComponent: Link })}
-    >
-      <ListItemIcon
-        sx={{
-          minWidth: 0,
-          mr: open ? 3 : "auto",
-          justifyContent: "center",
-        }}
-      >
-        <Icon color={active ? "primary" : "inherit"} />
-      </ListItemIcon>
+}: DrawerListItemProps) => {
+  return (
+    <ListItem disablePadding sx={{ display: "block", p: 1 }}>
+      <WrapperLink href={href}>
+        <ListItemButton
+          sx={{
+            minHeight: 48,
+            justifyContent: open ? "initial" : "center",
+            px: 1.5,
+          }}
+          selected={active}
+          onClick={onClick}
+        >
+          <ListItemIcon
+            sx={{
+              minWidth: 0,
+              mr: open ? 3 : "auto",
+              justifyContent: "center",
+            }}
+          >
+            <Icon color={active ? "primary" : "inherit"} />
+          </ListItemIcon>
 
-      <ListItemText
-        sx={{
-          whiteSpace: "normal",
-          opacity: open ? 1 : 0,
-        }}
-        primary={text}
-        primaryTypographyProps={{
-          color: active ? "primary" : "inherit",
-        }}
-      />
-    </ListItemButton>
-  </ListItem>
-);
+          <ListItemText
+            sx={{
+              whiteSpace: "normal",
+              opacity: open ? 1 : 0,
+            }}
+            primary={text}
+            primaryTypographyProps={{
+              color: active ? "primary" : "inherit",
+            }}
+          />
+        </ListItemButton>
+      </WrapperLink>
+    </ListItem>
+  );
+};
 
 export default DrawerListItem;
