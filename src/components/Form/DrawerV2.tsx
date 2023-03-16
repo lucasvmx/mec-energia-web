4f1c2442fe2da3ecf147c3c18ff254c2534b3581
Irwin Schmitt <irwinschmitt@gmail.com>
2023-03-16 14:20:37 -0300

    feat(form): create example drawer on /form-example

---
+++ src/components/Form/DrawerV2.tsx
@@ -0,0 +1,86 @@
+import { ReactNode } from "react";
+import {
+  AppBar,
+  Box,
+  Container,
+  Drawer,
+  IconButton,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { CloseRounded } from "@mui/icons-material";
+
+interface FormDrawerV2Props {
+  open: boolean;
+  title: string;
+  header: ReactNode;
+  sections: ReactNode[];
+  footer: ReactNode;
+  handleCloseDrawer: () => void;
+}
+
+const FormDrawerV2 = ({
+  open,
+  title,
+  header,
+  sections,
+  footer,
+  handleCloseDrawer,
+}: FormDrawerV2Props) => {
+  return (
+    <Drawer
+      open={open}
+      anchor="bottom"
+      PaperProps={{ sx: { height: "100%" } }}
+      onClose={handleCloseDrawer}
+    >
+      <AppBar position="static">
+        <Toolbar>
+          <Container maxWidth="md">
+            <Box display="flex" alignItems="center">
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="Fechar formulário"
+                onClick={handleCloseDrawer}
+              >
+                <CloseRounded />
+              </IconButton>
+
+              <Box ml={3}>
+                <Typography variant="h6">{title}</Typography>
+              </Box>
+            </Box>
+          </Container>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="sm">
+        <Box mt={3} mb={6}>
+          {header}
+
+          <Box my={4}>
+            <Typography variant="body2" color="primary.main">
+              * campos obrigatórios
+            </Typography>
+          </Box>
+
+          <Box mt={4}>
+            {sections.map((section, index) => (
+              <Box mt={4} key={index}>
+                <Paper>
+                  <Box p={2}>{section}</Box>
+                </Paper>
+              </Box>
+            ))}
+          </Box>
+
+          <Box mt={4}>{footer}</Box>
+        </Box>
+      </Container>
+    </Drawer>
+  );
+};
+
+export default FormDrawerV2;
---
+++ src/pages/form-example.tsx
@@ -0,0 +1,180 @@
+import type { NextPage } from "next";
+
+import FormDrawerV2 from "@/components/Form/DrawerV2";
+import { Fragment, useState } from "react";
+import { Alert, Box, Button, Grid, TextField, Typography } from "@mui/material";
+
+const Header = () => (
+  <>
+    <Typography variant="h4">Subgrupo A4</Typography>
+
+    <Box mt={1}>
+      <Typography variant="body2">Distribuidora: Neoenergia</Typography>
+    </Box>
+
+    <Box mt={4}>
+      <Alert>
+        Siga <u>este passo-a-passo</u> para mais informações.
+      </Alert>
+    </Box>
+  </>
+);
+
+const SectionOne = () => (
+  <>
+    <Typography variant="h5">Seção 1</Typography>
+
+    <Grid container pt={2}>
+      <Grid item xs={6}>
+        <TextField />
+      </Grid>
+
+      <Grid item xs={6}>
+        <TextField />
+      </Grid>
+    </Grid>
+  </>
+);
+
+const SectionTwo = () => (
+  <>
+    <Typography variant="h5">Seção 2</Typography>
+
+    <Grid container pt={2} spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant="body2" color="primary.main">
+          NA
+        </Typography>
+      </Grid>
+
+      <Grid item xs={6}>
+        <TextField />
+      </Grid>
+
+      <Grid item xs={6}>
+        <TextField />
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="body2" color="primary.main">
+          Ponta
+        </Typography>
+      </Grid>
+
+      <Grid item xs={6}>
+        <TextField />
+      </Grid>
+    </Grid>
+  </>
+);
+
+const SectionThree = () => (
+  <>
+    <Typography variant="h5">Seção 3</Typography>
+
+    <Grid container pt={2} spacing={2}>
+      <Grid item xs={12} container>
+        <Grid item xs={3}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={4}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={5}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={6}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={7}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={8}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={9}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={10}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={11}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={12} container>
+        <Grid item xs={12}>
+          <TextField fullWidth />
+        </Grid>
+      </Grid>
+    </Grid>
+  </>
+);
+
+const Footer = () => (
+  <>
+    <Alert>Corrija os erros acima antes de gravar</Alert>
+
+    <Box mt={1} display="flex">
+      <Button variant="contained">Gravar</Button>
+
+      <Box ml={1}>
+        <Button variant="outlined">Cancelar</Button>
+      </Box>
+    </Box>
+  </>
+);
+
+const FormExamplePage: NextPage = () => {
+  const [open, setOpen] = useState(true); // provavelmetne vc vai salvar no redux ao inves do useState
+
+  const handleCloseDrawer = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Fragment>
+      <Button onClick={() => setOpen(true)}>Abrir formulário</Button>
+
+      <FormDrawerV2
+        open={open}
+        title="Lançar tarifas"
+        header={<Header />}
+        sections={[
+          <SectionOne key={0} />,
+          <SectionTwo key={1} />,
+          <SectionThree key={2} />, // faz um map pra adicionar essa key, talvez
+        ]}
+        footer={<Footer />}
+        handleCloseDrawer={handleCloseDrawer}
+      />
+    </Fragment>
+  );
+};
+
+export default FormExamplePage;
