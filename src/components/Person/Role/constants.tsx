a55cb16ab9ff85fda287fd45dd4379d64e688b45
Irwin Schmitt <irwinschmitt@gmail.com>
2023-03-22 07:49:39 -0300

    fix: user role chip tooltip

--- src/components/Person/Role/constants.tsx
+++ src/components/Person/Role/constants.tsx
@@ -8,8 +8,8 @@ export const UserRoleLabelMap = {
 
 export const UserRoleDescriptionMap = {
   [UserRole.UNIVERSITY_ADMIN]:
-    "O perfil “Operacional” tem acesso às tarefas básicas do sistema como:  gerenciar unidades consumidoras e distribuidoras, lançar faturas e tarifas, além de gerar recomendações.",
-  [UserRole.UNIVERSITY_USER]:
     "O perfil “Gestão” permite gerenciar o perfil das outras pessoas que usam o sistema. Além das tarefas operacionais como lançar faturas e gerar recomendações.",
+  [UserRole.UNIVERSITY_USER]:
+    "O perfil “Operacional” tem acesso às tarefas básicas do sistema como:  gerenciar unidades consumidoras e distribuidoras, lançar faturas e tarifas, além de gerar recomendações.",
   [UserRole.SUPER_USER]: "Super Admin",
 };
